1f49bf72584f314edff7392be3f2b83a996273be
Matagi <31905238+XRayZen@users.noreply.github.com>
2023-10-10 12:08:01 +0900

    Add genMock

--- src/infra/repository/apiRepo.tsx
+++ src/infra/repository/apiRepo.tsx
@@ -2,34 +2,48 @@ interface IUserApiRepo {
   isMOck: boolean;
 
   genUserID: () => Promise<string>;
-  RegisterUser(userID: string,userConfig: UserConfig): Promise<string>;
+  RegisterUser(userID: string, userConfig: UserConfig): Promise<string>;
   ConfigSync(userID: string): Promise<ConfigSyncResponse>;
-  ReportReadActivity(userID: string,readHistory: ReadHistory): Promise<string>;
-  UpdateUiConfig(userID: string,clientConfig: ClientConfig): Promise<string>;
+  ReportReadActivity(userID: string, readHistory: ReadHistory): Promise<string>;
+  UpdateUiConfig(userID: string, clientConfig: ClientConfig): Promise<string>;
 
-  ModifySearchHistory(userID: string,searchHistory: SearchHistory[],searchHistoryType: string): Promise<string>;
-  ModifyFavoriteSite(userID: string,favoriteSite: FavoriteSite[],favoriteSiteType: string): Promise<string>;
-  ModifyFavoriteArticle(userID: string,favoriteArticle: FavoriteArticle[],favoriteArticleType: string): Promise<string>;
+  ModifySearchHistory(
+    userID: string,
+    searchHistory: SearchHistory[],
+    searchHistoryType: string,
+  ): Promise<string>;
+  ModifyFavoriteSite(
+    userID: string,
+    favoriteSite: FavoriteSite[],
+    favoriteSiteType: string,
+  ): Promise<string>;
+  ModifyFavoriteArticle(
+    userID: string,
+    favoriteArticle: FavoriteArticle[],
+    favoriteArticleType: string,
+  ): Promise<string>;
 
   GetAPIRequestLimit(userID: string): Promise<ApiConfig>;
-  ModifyAPIRequestLimit(userID: string,apiConfig: ApiConfig): Promise<string>;
+  ModifyAPIRequestLimit(userID: string, apiConfig: ApiConfig): Promise<string>;
 
   DeleteUserData(userID: string): Promise<string>;
 }
 
 interface IRssApiRepo {
-  Search(userID: string,searchRequest:ApiSearchRequest): Promise<SearchResult>;
-  SubscribeSite(userID: string,site:WebSite,isSubscribe: boolean): Promise<string>;
-  FetchArticle(userID: string,fetchArticleRequest:FetchArticlesRequest): Promise<FetchArticleResponse>;
+  Search(userID: string, searchRequest: ApiSearchRequest): Promise<SearchResult>;
+  SubscribeSite(userID: string, site: WebSite, isSubscribe: boolean): Promise<string>;
+  FetchArticle(
+    userID: string,
+    fetchArticleRequest: FetchArticlesRequest,
+  ): Promise<FetchArticleResponse>;
 
   ExploreCategory(userID: string): Promise<ExploreCategory[]>;
-  ModifyExploreCategory(userID: string,exploreCategory:ExploreCategory,ModifyType: string): Promise<string>;
-  ChangeSiteCategory(userID: string,siteUrl:string,categoryName:string): Promise<string>;
+  ModifyExploreCategory(
+    userID: string,
+    exploreCategory: ExploreCategory,
+    ModifyType: string,
+  ): Promise<string>;
+  ChangeSiteCategory(userID: string, siteUrl: string, categoryName: string): Promise<string>;
 
-  DeleteSite(userID: string,siteUrl:string,isUnscoped:boolean): Promise<string>;
+  DeleteSite(userID: string, siteUrl: string, isUnscoped: boolean): Promise<string>;
 }
-
-
-
-
-
