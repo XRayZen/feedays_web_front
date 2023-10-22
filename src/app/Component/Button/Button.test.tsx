4238e72effd139bd3e850b7f49e67eafa890553e
Matagi <31905238+XRayZen@users.noreply.github.com>
2023-10-22 16:57:20 +0900

    install jest

--- src/app/Component/Button/Button.test.tsx
+++ src/app/Component/Button/Button.test.tsx
@@ -1,5 +1,5 @@
-import "@testing-library/jest-dom/extend-expect";
 import { render, screen, fireEvent } from "@testing-library/react";
+import '@testing-library/jest-dom'
 import Button from "./Button";
 
 describe("Buttonコンポーネントのテスト", () => {
