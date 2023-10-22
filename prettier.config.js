f703935b44e7784efc97c070735f9615ef9b9ba8
Matagi <31905238+XRayZen@users.noreply.github.com>
2023-10-22 15:56:12 +0900

    install Eslint+Prettier

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // 意味: 1行の最大文字数
+  printWidth: 100,
+  // 意味: インデントのスペース数
+  tabWidth: 2,
+  // 意味: タブを使用するか
+  useTabs: false,
+  // 意味: セミコロンを使用するか
+  semi: true,
+  // 意味: シングルクォートを使用するか
+  singleQuote: true,
+  // 意味:プロパティ名が識別子として有効な場合は引用符を付けず、それ以外の場合は引用符を付けることを意味します。
+  quoteProps: 'as-needed',
+  // 意味: JSXの最後の`>`を折り返すかどうか
+  trailingComma: 'es5',
+  // 意味: オブジェクトの{}の前後にスペースを入れるかどうか
+  endOfLine: 'lf',
+  // 意味: 空白文字を無視するかどうか
+  embeddedLanguageFormatting: 'off',
+};
